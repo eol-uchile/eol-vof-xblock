f1e45aa0a77010284213a71cb1e817adc93183b4
matsalinas <matias.salinas.s@mail.pucv.cl>
2019-10-15 19:34:04 +0000

    License

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ def package_data(pkg, roots):
 setup(
     name='vof-xblock',
     version='0.1',
-    description='vof XBlock',   # TODO: write a better description.
-    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    description='XBlock table with true/false questions',
+    license='AGPL v3',
     packages=[
         'vof',
     ],
