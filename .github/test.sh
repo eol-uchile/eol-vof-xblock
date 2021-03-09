01cb35b69187e619dc02ab83b7677b63e605348e
matsalinas <matsalinas@uchile.cl>
2021-03-09 13:18:49 +0000

    Update tests koa

--- .github/test.sh
+++ .github/test.sh
@@ -2,14 +2,14 @@
 
 pip install -e /openedx/requirements/eol-vof-xblock
 
-cd /openedx/requirements/eol-vof-xblock/vof
+cd /openedx/requirements/eol-vof-xblock
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/eol-vof-xblock/vof
+cd /openedx/requirements/eol-vof-xblock
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest vof/tests.py
 
 rm -rf test_root
